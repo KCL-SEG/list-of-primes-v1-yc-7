36ebf02032c209c584e960bca54121837b34ab8d
Yu Han Chen <yu.h.chen@kcl.ac.uk>
2022-10-02 11:46:37 +0100

    add solution

--- primes.py
+++ primes.py
@@ -1,6 +1,24 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+
+def isPrime(n):
+    if (n <= 0 or n==1):
+        return False
+
+    for i in range(2, n):
+        if (n % i) == 0:
+            return False
+    return True
+
+
 def primes(number_of_primes):
     list = []
+
+    number = 1
+    while len(list) < number_of_primes:
+        if isPrime(number):
+            list.append(number)
+        number += 1
+
     return list
